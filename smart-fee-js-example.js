28c43fa3a81c90ca553a6a85790b1b9a98bf5901
Danny Diekroeger <dannydiek@gmail.com>
2021-06-25 19:04:09 -0700

    Comments

--- smart-fee-js-example.js
+++ smart-fee-js-example.js
@@ -1,14 +1,15 @@
 const BitGoJS = require('bitgo')
 const smartFee = require('smart-fee-js')
 /**
- * TODO: Enter your SmartFee api key below. You can request one from hello@smartfee.live
+ * TODO: Set your SmartFee api key as an env variable or enter it below. 
+ * You can request one by emailing hello@smartfee.live.
  */
 const SMART_FEE_API_KEY = process.env.SMART_FEE_API_KEY || ''
 const SMART_FEE_ENV = smartFee.environments.STAGING
 // const SMART_FEE_ENV = smartFee.enviornments.PRODUCTION
 
 /**
- * TODO: Enter your BitGo wallet info below.
+ * TODO: Set your BitGo wallet env variables or enter them below.
  */
 const BITGO_ACCESS_TOKEN = process.env.BITGO_ACCESS_TOKEN || ''
 const WALLET_ID = process.env.BITGO_WALLET_ID || ''
@@ -16,7 +17,7 @@ const BITGO_WALLET_PASSWORD = process.env.WALLET_PASSWORD || ''
 
 /**
  * TODO: Add your batched withdrawal recipients to the list below.
- * Or you can leave it empty for testing.
+ * Or you can leave the list empty for testing.
  */
 const recipients = [{ address: 'tb1qwu9kscuqa39kj8jquwhuzcqttjtd43aqfxpjfyt6f2jw4ed2za8qmxp936', amount: 10000 }]
 //const recipients = []
@@ -36,7 +37,7 @@ async function createTransaction() {
     }
     // Use Smart Fee to generate the BitGo send parameters. This will attempt to create a transaction to your 
     // recipients with no change output, and one output to Smart Fee to be used for fee bumping. It will only
-    // create change outputs if it's a good time to split change
+    // create change outputs if you have targetWalletUnspents set and it's a good time to split change.
     const sendParams = await smartFee.generateBitGoSendParams(wallet, recipients, smartFeeOptions, SMART_FEE_ENV)
     
     // Send the transaction via BitGo.
